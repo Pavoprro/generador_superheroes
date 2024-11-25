b2fa3d79bc21575471cbc4b7e0f58b69499ac052
Pavoprro <194533-3@iberoleon.edu.mx>
2024-11-25 11:57:45 -0600

    Update superheroes.py
    generar nombre aleatorio

--- superheroes.py
+++ superheroes.py
@@ -1,3 +1,9 @@
+import random
+def generar_nombre_aleatorio():
+    base = random.choice(nombres_base)
+    sufijo = random.choice(sufijos)
+    return f"{base}{sufijo}"
+
 def generar_nombre(base,sufijo):
     return f"{base}{sufijo}"
 
