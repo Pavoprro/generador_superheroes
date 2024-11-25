96b0336a4061076924d0cc65bb3dd387d318048f
Pavoprro <194533-3@iberoleon.edu.mx>
2024-11-25 11:30:59 -0600

    Update superheroes.py
    Agregados los nombres de los superheroes
    
    Update superheroes.py
    
    Agregados nombres y sufijos

--- superheroes.py
+++ superheroes.py
@@ -3,3 +3,6 @@ def generar_nombre(base,sufijo):
 
 def asignar_habilidad(nombre, habilidad):
     return f"{nombre}tiene el superpoder de {habilidad}"
+
+nombre_base=["el bicho","skibidi", "que", "si"]
+sufijos=["siuuu","toile", "so", "no"]
