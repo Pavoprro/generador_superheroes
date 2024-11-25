748b049834117b5e6ce9b16172eb537ea82e4833
Pavoprro <194533-3@iberoleon.edu.mx>
2024-11-25 11:30:59 -0600

    Update superheroes.py
    Agregados los nombres de los superheroes

--- superheroes.py
+++ superheroes.py
@@ -3,3 +3,5 @@ def generar_nombre(base,sufijo):
 
 def asignar_habilidad(nombre, habilidad):
     return f"{nombre}tiene el superpoder de {habilidad}"
+
+nombre_base=["Sombra","Guardian", "Halcon", "Fenix"]
